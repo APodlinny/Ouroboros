4389fc1f0620d34ab9e5bb190dad4c6989727e3c
Alexander Podlinny <apodlinny@gmail.com>
2012-10-28 23:51:33 +0200

    Changing inner representation scheme of bench files
    Using edges list representation instead of matrix representation.

--- src/Ouroboros/Scheme/FromScheme.hs
+++ src/Ouroboros/Scheme/FromScheme.hs
@@ -6,21 +6,4 @@ import Ouroboros.Scheme.Definition
 import qualified Ouroboros.Language as AST
 
 programFromScheme :: Scheme -> AST.Program
-programFromScheme (Scheme schemeName matrix nodes) = AST.Program textLines
-	where
-		textLines = (AST.Comment schemeName) : codeLines
-		codeLines = map makeCodeLine commands
-		makeCodeLine command = AST.Code command AST.Empty
-		commands = getCommands matrix nodes
-        
-getCommands :: ConnectionMatrix -> [Node] -> [AST.Command]
-getCommands matrix nodes = getInputs matrix nodes :
-						   getOutputs matrix nodes :
-						   getDefinitions matrix nodes
-	where
-		getInputs = undefined
-		getOutputs = undefined
-        
-getDefinitions :: ConnectionMatrix -> [Node] -> [AST.Command]
-getDefinitions = undefined
-
+programFromScheme = undefined
